2507694e97304ef3aabd8cee34fcf02604486a37
HiIamJeff67 <iamjeffhi67@gmail.com>
2025-09-16 14:44:13 +0800

    fix: fix some constraints in sub shelf

--- app/models/schemas/sub_shelf_schema.go
+++ app/models/schemas/sub_shelf_schema.go
@@ -12,7 +12,7 @@ type SubShelf struct {
 	Id             uuid.UUID   `json:"id" gorm:"column:id; type:uuid; primary; default:gen_random_uuid();"`
 	Name           string      `json:"name" gorm:"column:name; size:128; not null; default:'undefined'; uniqueIndex:sub_shelf_idx_name_root_shelf_id_path;"`
 	RootShelfId    uuid.UUID   `json:"rootShelfId" gorm:"column:root_shelf_id; type:uuid; not null; uniqueIndex:sub_shelf_idx_name_root_shelf_id_path;"`
-	PrevSubShelfId *uuid.UUID  `json:"prevSubShelfId" gorm:"column:prev_sub_shelf_id; type:uuid;"`
+	PrevSubShelfId *uuid.UUID  `json:"prevSubShelfId" gorm:"column:prev_sub_shelf_id; type:uuid; check:prev_sub_shelf_id_check,prev_sub_shelf_id != id;"`
 	Path           []uuid.UUID `json:"path" gorm:"column:path; type:uuid[]; not null; check:path_length_check,length(path) >= 0 AND length(path) <= 100; uniqueIndex:sub_shelf_idx_name_root_shelf_id_path;"`
 	DeletedAt      time.Time   `json:"deletedAt" gorm:"column:deleted_at; type:timestamptz;"`
 	UpdatedAt      time.Time   `json:"updatedAt" gorm:"column:updated_at; type:timestamptz; not null; autoUpdateTime:true;"`
@@ -25,8 +25,8 @@ type SubShelf struct {
 }
 
 // contraints:
-// 1. Name + ParentShelfId : In the current SubShelf, it should be unique one their names
-// 2. RootShelfId + Path : In the current SubShelf, the path from the root to it should be unique
+// 1. SubShelf.PrevSubShelfId != SubShelf.Id (Do NOT point to itself)
+// 2. Use Name + RootShelfId + Path as the unique constraints (That means in some specific layers of a RootShelf, the name of a SubShelf should be unique)
 
 func (SubShelf) TableName() string {
 	return types.ValidTableName_SubShelfTable.String()
